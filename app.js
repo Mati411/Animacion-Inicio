0ed1afa5dd34ec9cdc053a503efe5edcc302062a
Mati411 <matiasgarcia1204@gmail.com>
2022-03-29 12:23:27 -0300

    Animacion de inicio

---
+++ app.js
@@ -0,0 +1,16 @@
+const cortina = document.querySelector('.cortina');
+const border = document.querySelector('.intro-borde');
+const backgroundLogo = document.querySelector('.intro-circulo');
+const logo = document.querySelector('.intro-titulo');
+const navbar = document.querySelector('.navbar');
+const textoPostIntro = document.querySelector('.intro-texto');
+
+
+setTimeout(() => {
+    cortina.style.animation = 'cortinaIntro 1s ease-in-out forwards';
+    backgroundLogo.style.animation = 'unsize 1.5s ease-in-out forwards';
+    border.style.animation = 'fadeOut .5s ease-in-out forwards';
+    // logo.style.animation = 'resizeTitulo 1.5s ease-in-out forwards';
+    navbar.style.animation = 'fadeIn 2s ease-in-out forwards';
+    textoPostIntro.style.animation = 'fadeIn 2.5s ease-in-out forwards';
+}, 2450);
