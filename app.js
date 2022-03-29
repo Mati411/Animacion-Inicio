49ad159f6469a54499c674c49c213d3693c02e50
Mati411 <matiasgarcia1204@gmail.com>
2022-03-29 12:41:32 -0300

    FIX

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ setTimeout(() => {
     cortina.style.animation = 'cortinaIntro 1s ease-in-out forwards';
     backgroundLogo.style.animation = 'unsize 1.5s ease-in-out forwards';
     border.style.animation = 'fadeOut .5s ease-in-out forwards';
-    // logo.style.animation = 'resizeTitulo 1.5s ease-in-out forwards';
+    logo.style.animation = 'resizeTitulo 1.5s ease-in-out forwards';
     navbar.style.animation = 'fadeIn 2s ease-in-out forwards';
     textoPostIntro.style.animation = 'fadeIn 2.5s ease-in-out forwards';
 }, 2450);
